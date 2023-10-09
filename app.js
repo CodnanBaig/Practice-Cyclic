ca2ded88eca4aacddb9e255cf39bd2fd82046fc2
CodnanBaig <adnanbaigofficial@gmail.com>
2023-10-09 16:20:41 +0530

    Cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const todoRoutes = require("./routes/todoRoutes");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api", todoRoutes);
